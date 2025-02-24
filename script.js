5b8b99f73ecc738a17f61b5666d228dfe4e4abff
SirGrimm254 <alexandergathendo666@gmail.com>
2025-02-24 03:24:48 +0300

    commit

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function openPopup() {
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
+
+window.onclick = function(event) {
+    var popup = document.getElementById('popup');
+    if (event.target === popup) {
+        closePopup();
+    }
+}
