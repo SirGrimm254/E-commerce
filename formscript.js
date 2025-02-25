18fd67c1c472ad246347f8d3db85fb9c1db273f0
SirGrimm254 <alexandergathendo666@gmail.com>
2025-02-25 23:24:11 +0300

    commit

--- formscript.js
+++ formscript.js
@@ -11,3 +11,63 @@ function validateRange(input, min, max) {
 function isNumber(event) {
     return event.charCode >= 48 && event.charCode <= 57;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("JavaScript Loaded!");
+
+    document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log("Form Submitted!");
+
+        const username = document.getElementById("username").value;
+        const password =  document.getElementById("password").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
+
+        const passwordRegex = /^[A-Z][A-Za-z0-9]{7,14}$/;
+
+        if (!username || !password || !confirmPassword) {
+            alert("All fields required!");
+        }
+
+        if (!passwordRegex.test(password)) {
+            alert("Password must start with a capital letter, be 8-15 characters long and contain only letters and numbers.");
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+
+        try {
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+            alert("Registration successful! You can now log in.");
+            window.location.href = "login.html";
+        } catch (error) {
+            console.error("Error saving to localStorage:", error);
+            alert("An error occurred. please try again.");
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+
+    document.querySelector(".btn input[type='button']").addEventListener("click", function () {
+
+        const enteredUsername = document.getElementById("username").value;
+        const enteredPassword = document.getElementById("password").value;
+
+        const storedUsername = localStorage.getItem("username");
+        const storedPassword = localStorage.getItem("password");
+
+        if (enteredUsername === storedUsername && enteredPassword === storedPassword) {
+            alert("Login Successful!");
+            window.location.href = "index.html";
+        } else {
+            alert("Invalid username or password!");
+        }
+    });
+});
